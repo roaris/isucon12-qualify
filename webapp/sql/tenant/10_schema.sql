fc5cc00e51f10975ba65a4253e5a4bf0b611e7a2
roaris <one0803kyopro@gmail.com>
2023-09-03 10:40:10 +0000

    player_scoreテーブルのインデックス修正

--- webapp/sql/tenant/10_schema.sql
+++ webapp/sql/tenant/10_schema.sql
@@ -31,5 +31,4 @@ CREATE TABLE player_score (
   updated_at BIGINT NOT NULL
 );
 
-CREATE INDEX player_score_player_id_index ON player_score(player_id);
-CREATE INDEX player_score_competition_id_index ON player_score(competition_id);
+CREATE INDEX player_score_tenant_id_and_competition_id_index ON player_score(tenant_id, competition_id);
